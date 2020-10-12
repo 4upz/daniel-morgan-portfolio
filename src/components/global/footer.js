d2b45a719d6b6c17e6ae49599181ded291e4540c
4upz <ams382@case.edu>
2020-10-12 13:24:52 -0400

    Add footer styling and content

--- src/components/global/footer.js
+++ src/components/global/footer.js
@@ -1,5 +1,14 @@
 import React from "react"
 
-const Footer = () => <h1>Hello World</h1>
+const Footer = () => (
+    <footer className="footer">
+        <div className="social-links">
+            <p>Instagram</p>
+        </div>
+        <h1>DSMJR</h1>
+        Copyright © {new Date().getFullYear()} Daniel Morgan Jr., All Rights
+        Reserved
+    </footer>
+)
 
 export default Footer
--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import Wrap from "../shared/wrap"
 import "../../styles/global.scss"
 
@@ -29,16 +30,8 @@ const Layout = ({ children }) => {
             <Header />
             <Wrap>
                 <main>{children}</main>
-                <footer
-                    style={{
-                        marginTop: `2rem`,
-                    }}
-                >
-                    © {new Date().getFullYear()}, Built with
-                    {` `}
-                    <a href="https://www.gatsbyjs.com">Gatsby</a>
-                </footer>
             </Wrap>
+            <Footer />
         </>
     )
 }
