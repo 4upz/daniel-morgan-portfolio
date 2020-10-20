613f7e522b91c93ce1c6d0565eccf59d3705e081
Arik Smith <ariksmith@unionhomemortgage.com>
2020-10-20 15:31:26 -0400

    Add support for background images

--- src/components/backgroundImage.js
+++ src/components/backgroundImage.js
@@ -1,12 +1,10 @@
 import React from "react"
-import PropTypes from "prop-types"
-import CTACard from "../cards/ctaCard"
-import Container from "../shared/container"
 
 import BackgroundImage from "gatsby-background-image"
 import { StaticQuery } from "gatsby"
 
-const TileBackground = ({ children }) => (
+// Just an example of how to use background image
+const BackgroundImage = ({ className }) => (
     <StaticQuery
         query={graphql`
             query {
@@ -25,34 +23,13 @@ const TileBackground = ({ children }) => (
             return (
                 <BackgroundImage
                     Tag="section"
-                    className="tile"
+                    className={className}
                     fluid={imageData}
                     backgroundColor={`#040e18`}
                 >
-                    {children}
+                    <h2>gatsby-background-image</h2>
                 </BackgroundImage>
             )
         }}
     />
 )
-
-const Tile = ({ title, description, ctaText, ctaLink }) => (
-    <Container className="tile">
-        <Container className="tile-content vertical-center">
-            <h2>{title}</h2>
-            <CTACard
-                cardText={description}
-                buttonText={ctaText}
-                buttonLink={ctaLink}
-            />
-        </Container>
-    </Container>
-)
-
-Tile.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    ctaText: PropTypes.string.isRequired,
-}
-
-export default Tile
