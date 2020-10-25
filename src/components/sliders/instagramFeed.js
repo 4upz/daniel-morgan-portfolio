210a6d7d6d0aa78c2a026f07679ea62284f58f8a
4upz <ams382@case.edu>
2020-10-25 15:01:03 -0400

    Style adjustments on iFrame

--- src/components/sliders/instagramFeed.js
+++ src/components/sliders/instagramFeed.js
@@ -6,15 +6,15 @@ const InstagramFeed = () => (
         <h1>Social Media</h1>
         {/* TODO" Convert to Gatsby/React Responsive iFrame */}
         <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-        <iframe
-            title="IGFeed"
-            src="//lightwidget.com/widgets/c2eb0c27b9ac5e16994ad7d17f94d21b.html"
-            scrolling="no"
-            allowtransparency="true"
-            className="lightwidget-widget"
-            width="600"
-            height="400"
-        ></iframe>
+        <Container className="widget">
+            <iframe
+                title="IGFeed"
+                src="//lightwidget.com/widgets/c2eb0c27b9ac5e16994ad7d17f94d21b.html"
+                scrolling="no"
+                allowtransparency="true"
+                className="lightwidget-widget"
+            ></iframe>
+        </Container>
     </Container>
 )
 
