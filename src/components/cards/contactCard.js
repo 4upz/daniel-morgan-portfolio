f0742c3bfcdf2349b459b7df3a2d42a7ec37f170
Arik Smith <ariksmith@unionhomemortgage.com>
2020-10-26 17:22:27 -0400

    Finished contact form with netlify fields

--- src/components/cards/contactCard.js
+++ src/components/cards/contactCard.js
@@ -8,13 +8,19 @@ const ContactCard = () => (
         <Card className="contact">
             <p>
                 I'm always open to potential partnernships, networking
-                opportunities, or even a friendly chat.
-                <br />
-                Fill out the form below and I'll get back to you as soon as I
-                can!
+                opportunities, or even a friendly chat. Fill out the form below
+                and I'll get back to you as soon as I can!
             </p>
             {/* TODO: Change these labels to use the "for" attribute so labels can be styled */}
-            <form className="contact-form" method="post" action="#">
+            <form
+                className="contact-form"
+                method="post"
+                netlify-honeypot="bot-field"
+                data-netlify="true"
+                name="contact"
+            >
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />
                 <label className="name">
                     Name
                     <input type="text" name="name" id="name" />
