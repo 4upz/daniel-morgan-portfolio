b59730b9e192f97144faf55a8abd6ad321901384
4upz <ams382@case.edu>
2020-11-03 17:53:00 -0500

    Re-work quote cards and revise quote content

--- src/utils/reviews.js
+++ src/utils/reviews.js
@@ -11,7 +11,7 @@ export const reviews = [
     },
     {
         content:
-            "Daniel Morgan Jr is not only an exceptional leader, but he is an individual who possesses such great artistic qualities. Daniel has proven himself to be a great listener, and also a wonderful person to seek advice from.",
+            "Daniel Morgan Jr. is not only an exceptional leader, but he is an individual who possesses such great artistic qualities. Daniel has proven himself to be a great listener, and also a wonderful person to seek advice from.",
         author: "Giade Ensley",
     },
     {
@@ -21,7 +21,7 @@ export const reviews = [
     },
     {
         content:
-            "Daniel Morgan Jr is one of the most hardworking, punctual, and focused individuals I’ve ever met!  He is a pleasure to work with and very easy going.  He’s very open-minded and open to all beliefs, and would still love you nonetheless.",
+            "Daniel Morgan Jr. is one of the most hardworking, punctual, and focused individuals I’ve ever met!  He is a pleasure to work with and very easy going.  He’s very open-minded and open to all beliefs, and would still love you nonetheless.",
         author: "Brayon Miller",
     },
     {
@@ -32,5 +32,50 @@ export const reviews = [
     {
         content: "He’s a good friend.",
         author: "Leiderly Martinez",
+    },
+    {
+        content:
+            "This is a talented, smart, and handsome young man! May life bring him the joy he has given to the world.",
+        author: "Pier and L.T. Randle",
+    },
+    {
+        content:
+            "Daniel is a wonderful young man of God who is destined for great things.",
+        author: "Alexis Tate",
+    },
+    {
+        content:
+            "Danny is one of the best people I have worked with both as a college student and a friend. When he sets his mind to something, he does it, he’s a real doer. Working with him for the past few years has been one of the best experiences I have had in a long time, when it comes to getting things done effectively, efficiently, and organized. I would recommend Danny to anyone when it comes to needing a good worker and a leader.",
+        author: "Samuel Sawyer",
+    },
+    {
+        content:
+            "Daniel, a man of integrity. To me, this means I can count on him if I need to bounce a business idea off his brain or talk from a spiritual point of view. He has a kind spirit and has a strong presence yet a gentle voice. He is not afraid to voice his opinions on any subject we discuss. When I’m talking to Daniel, it’s like speaking to one of my peers hard to believe he’s just 18. We share a love for God, fashion, and entrepreneurship. I often look at him in a room and think to myself ‘Daniel is a living legend",
+        author: "Toka Ivey",
+    },
+    {
+        content:
+            "Dan is very ambitious, smart, creative, gentle, but aggressive. Whatever he wants he gets and nothing can stop him. He’s caring, happy, mature, and has a lot going for himself at a very young age. I enjoyed working with Dan and I am still very grateful to have had the chance to work with him. He takes control. The only thing I will say is Dan needs to practice patience because when he jumps he jumps. Everything is baby steps.",
+        author: "Makayla Freeman",
+    },
+    {
+        content:
+            "Daniel Morgan Jr.,, or Dan the man, is a super great guy. He is very determined and can work countless hours until he knows what he needs to be done is done. He is very smart and still loves to gain knowledge in any form he can. Daniel is also very reliable.",
+        author: "Stephon Harrison",
+    },
+    {
+        content:
+            "Dan is the extroverted multitalented man who has a bright future ahead of him. I’m 22 years old and felt as if Dan has surpassed me in the age of how knowledgeable and educated he is. I think he is very tedious and enjoys helping others with his input. He can be a tad bit controlling, but that’s anyone who sees the best in something. If I had a chance to work with Dan again it’d be a pleasure.",
+        author: "Makenzie Freeman",
+    },
+    {
+        content:
+            "In the short amount of time I’ve known Dan, he has shown to be diligent, passionate, and kind-hearted. Dan is a businessman. He looks to the future and becomes inspired by the world around him. Abhijit Naskar once stated, “It is only your dedication towards our passion that distinguishes a champion from the crowd.” In conclusion, Dan, keep being who you are because you are growing to be a great boss and a wonderful person.",
+        author: "Katrice McNair",
+    },
+    {
+        content:
+            "Daniel is a great entrepreneur. When he wants to achieve something he goes for it. What I admire most about him is he is not afraid to admit failure, and he also does not let it stop him from moving forward.",
+        author: "Jaila Bratton",
     },
 ]
